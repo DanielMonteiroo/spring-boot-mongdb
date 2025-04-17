4fd7b5edd7a18b6b59ce614cbc338ce1710c2255
Daniel <danieel.ios90@gmail.com>
2025-04-17 16:00:19 -0300

    Obtendo usuario por ID

--- src/main/java/com/danielmonteiro/workshopmongo/resources/UserResource.java
+++ src/main/java/com/danielmonteiro/workshopmongo/resources/UserResource.java
@@ -1,11 +1,13 @@
 package com.danielmonteiro.workshopmongo.resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.danielmonteiro.workshopmongo.domain.User;
@@ -21,13 +23,20 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
+	//Busca por todos
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll(){
 				
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(x-> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
-		
+	}
+	
+	//Busca por ID
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		Optional<User> obj = service.findById(id);		
+		return ResponseEntity.ok().body(new UserDTO(obj.get()));	
 	}
 	
 }
--- src/main/java/com/danielmonteiro/workshopmongo/services/UserService.java
+++ src/main/java/com/danielmonteiro/workshopmongo/services/UserService.java
@@ -1,12 +1,13 @@
 package com.danielmonteiro.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.danielmonteiro.workshopmongo.domain.User;
 import com.danielmonteiro.workshopmongo.repository.UserRepository;
+import com.danielmonteiro.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,4 +20,11 @@ public class UserService {
 		
 	}
 	
+	public Optional<User> findById(String id) {
+		Optional<User> user = repo.findById(id);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado!");
+		}
+		return user;
+	}
 }
